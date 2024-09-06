d60fe262db37e9e88ab93ab838694dca18efea9d
Jarno Sijben <91888926+ClosingYard@users.noreply.github.com>
2024-09-06 11:36:33 +0200

    .

--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
-const useSecondaryServer = process.env.REACT_APP_USE_SECONDARY_SERVER === 'true'; // You can toggle this in your .env file
-
+// config.js
 const config = {
-    baseURL: useSecondaryServer 
-        ? (process.env.REACT_APP_SECONDARY_API_URL || 'https://spa-six-rho.vercel.app/api') // Secondary server URL
-        : (process.env.REACT_APP_API_URL || 'http://192.168.178.86:5000/api'), // Primary server URL
+    baseURL: process.env.REACT_APP_API_URL || 'http://192.168.1.96:5000/api',
 };
 
 export default config;
